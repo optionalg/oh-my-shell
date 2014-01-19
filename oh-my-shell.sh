93283277d05d8fd610cd376375518b3450a8a1be
Sean Jones <neuralsandwich@gmail.com>
2014-01-19 02:23:45 +0000

    Starting cross shell theme

--- oh-my-shell.sh
+++ oh-my-shell.sh
@@ -1,6 +1,20 @@
-#!/usr/bin/env bash
-
 # Check for updates on initial load...
 if [ "$OHMYSHELL_DISABLE_AUTO_UPDATE" != "true" ]; then
-  $OHMYSHELL/tools/check_for_upgrade.sh
+  /usr/bin/env bash $OHMYSHELL/tools/check_for_upgrade.sh
+fi
+
+CURSHELL=`echo $SHELL| sed 's/.*\/bin\///'`
+
+# Set OHMYSHELL_CUSTOM to the path where your custom config files
+# and plugins exists, or else we will use the default custom/
+if [[ -z "$OHMYSHELL_CUSTOM" ]] ; then
+    OHMYSHELL_CUSTOM="$OHMYSHELL/custom"
+fi
+
+if [[ $CURSHELL -eq "zsh" ]] ;
+then
+	source $OHMYSHELL/oh-zsh.sh
+elif [[ $CURSHELL -eq "bash" ]] ;
+then
+	source $OHMYSHELL/oh-bash.sh
 fi
